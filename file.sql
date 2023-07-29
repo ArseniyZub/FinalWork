9dfb4a770e880b071bc4f65ba1070fc278dc327c
ArseniyZub <96389438+ArseniyZub@users.noreply.github.com>
2023-07-30 00:21:46 +0300

    Update file.sql

--- file.sql
+++ file.sql
@@ -1,53 +1,53 @@
 USE peopleFriends;
 
 CREATE TABLE cat (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	animal_name CHAR(30),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    animal_name CHAR(30),
     commands TEXT,
     date_of_birth DATE
 );
 
 CREATE TABLE dog (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	animal_name CHAR(30),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    animal_name CHAR(30),
     commands TEXT,
     date_of_birth DATE
 );
 
 CREATE TABLE hamster (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	animal_name CHAR(30),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    animal_name CHAR(30),
     commands TEXT,
     date_of_birth DATE
 );
 
 CREATE TABLE horse (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	animal_name CHAR(30),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    animal_name CHAR(30),
     commands TEXT,
     date_of_birth DATE
 );
 
 CREATE TABLE camel (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	animal_name CHAR(30),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    animal_name CHAR(30),
     commands TEXT,
     date_of_birth DATE
 );
 
 CREATE TABLE donkey (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	animal_name CHAR(30),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    animal_name CHAR(30),
     commands TEXT,
     date_of_birth DATE
 );
 
-INSERT INTO cat (animal_name,commands, date_of_birth) VALUES 
-    ('dfga', 'meow', '2021-01-01'),
-    ('lfdsk', 'meow, stand', '2019-12-10'),
-    ('frsdf', 'meow, wlow', '2020-02-02'),
-    ('dir', 'meow', '2022-03-03'),
-    ('dsfkl', 'meow', '2018-05-05');
+INSERT INTO cat (animal_name, commands, date_of_birth) VALUES 
+    ('1', 'meow', '2021-01-01'),
+    ('2', 'meow, stand', '2019-12-10'),
+    ('3', 'meow, wlow', '2020-02-02'),
+    ('4', 'meow', '2022-03-03'),
+    ('5', 'meow', '2018-05-05');
    
 INSERT INTO dog (animal_name,commands, date_of_birth) VALUES 
     (',ask', 'flufy', '2021-01-01'),
@@ -57,18 +57,18 @@ INSERT INTO dog (animal_name,commands, date_of_birth) VALUES
     ('dfls', 'meow', '2018-05-05');
     
 INSERT INTO hamster (animal_name,commands, date_of_birth) VALUES 
-    ('crack', 'eat', '2021-01-01'),
-    ('jisus', 'eat', '2019-12-10'),
-    ('qwerty', 'left hand', '2020-02-02'),
-    ('asdfg', 'right hand', '2022-03-03'),
-    ('black', 'meow', '2018-05-05');
+    ('klfg', 'eat', '2021-01-01'),
+    ('jhgfj', 'eat', '2019-12-10'),
+    ('qjt', 'left hand', '2020-02-02'),
+    ('asfgh', 'right hand', '2022-03-03'),
+    ('kdfl', 'meow', '2018-05-05');
     
 INSERT INTO horse (animal_name,commands, date_of_birth) VALUES 
-    ('igogo', 'eat', '2021-01-01'),
-    ('igaga', 'eat', '2019-12-10'),
-    ('ijoho', 'left hand', '2020-02-02'),
-    ('jijij', 'right hand', '2022-03-03'),
-    ('aoaoaa', 'meow', '2018-05-05');
+    ('klmdfg', 'eat', '2021-01-01'),
+    ('fgd', 'eat', '2019-12-10'),
+    ('hdf', 'left hand', '2020-02-02'),
+    ('wert', 'right hand', '2022-03-03'),
+    ('dfg', 'meow', '2018-05-05');
     
 INSERT INTO camel (animal_name,commands, date_of_birth) VALUES 
     ('qwea', 'eat', '2021-01-01'),
@@ -78,11 +78,11 @@ INSERT INTO camel (animal_name,commands, date_of_birth) VALUES
     ('werqr', 'meow', '2018-05-05');
     
 INSERT INTO donkey (animal_name,commands, date_of_birth) VALUES 
-    ('stup', 'eat', '2021-01-01'),
-    ('tupi', 'eat', '2019-12-10'),
-    ('upid', 'left hand', '2020-02-02'),
-    ('task', 'right hand', '2022-03-03'),
-    ('done', 'meow', '2018-05-05');
+    ('klnd', 'eat', '2021-01-01'),
+    ('fdgre', 'eat', '2019-12-10'),
+    ('dfgkl', 'left hand', '2020-02-02'),
+    ('tanrksk', 'right hand', '2022-03-03'),
+    ('fdgfkdl', 'meow', '2018-05-05');
     
 TRUNCATE camel;
 
@@ -123,15 +123,21 @@ INSERT INTO young_animal (animal_name, commands, date_of_birth, age)
 SELECT animal_name, commands, date_of_birth, age_animal(date_of_birth)
 FROM cat
 WHERE TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) BETWEEN 1 AND 3
+	
 UNION ALL
+	
 SELECT animal_name, commands, date_of_birth, age_animal(date_of_birth)
 FROM dog
 WHERE TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) BETWEEN 1 AND 3
+	
 UNION ALL
+	
 SELECT animal_name, commands, date_of_birth, age_animal(date_of_birth)
 FROM hamster
 WHERE TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) BETWEEN 1 AND 3
+	
 UNION ALL
+	
 SELECT animal_name, commands, date_of_birth, age_animal(date_of_birth)
 FROM horse_donkey
 WHERE TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) BETWEEN 1 AND 3;
